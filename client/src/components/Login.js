c1cef16e58d70862ef632520e159c416dcceef3d
Anit <anittankandasamy@gmail.com>
2022-11-28 00:29:54 -0500

    add oath2 to access token for scope yt readonly

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
 
+const CLIENT_ID =
+  '730391860568-5bqmr300dku394cucpfeeemcp6en9jp6.apps.googleusercontent.com';
+
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
 export default function Login(props) {
   const [user, setUser] = useState({});
 
@@ -20,15 +25,20 @@ export default function Login(props) {
 
   useEffect(() => {
     /* global google */
+    const google = window.google;
     google.accounts.id.initialize({
-      client_id:
-        '730391860568-5bqmr300dku394cucpfeeemcp6en9jp6.apps.googleusercontent.com',
+      client_id: CLIENT_ID,
       callback: handleCallbackResponse,
     });
 
     google.accounts.id.renderButton(document.getElementById('signInDiv'), {
       theme: 'outline',
       size: 'large',
+    });
+
+    google.accounts.oauth2.initTokenClient({
+      client_id: CLIENT_ID,
+      scope: SCOPES,
     });
 
     google.accounts.id.prompt();
